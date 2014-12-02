4e45940d056a78247f90170afe1b6485229b42cb
jIRKA <honza.jirman@gmail.com>
2014-12-02 21:33:07 +0100

    Renamed path stack to path queue

--- shared/src/main/java/lego/api/controllers/PlannedController.java
+++ shared/src/main/java/lego/api/controllers/PlannedController.java
@@ -13,14 +13,14 @@ public abstract class PlannedController extends MapAwareController {
     private static final byte X_BIT = (byte) 0x80;//1000_0000
     private static final byte Y_ANTIBIT = ~X_BIT; //0111_1111
 
-    private final BatchByteQueue pathStack = new BatchByteQueue(96);
+    private final BatchByteQueue pathQueue = new BatchByteQueue(96);
 
     public final void addXPath(byte x){
-        pathStack.add((byte) (x | X_BIT));
+        pathQueue.add((byte) (x | X_BIT));
     }
 
     public final void addYPath(byte y){
-        pathStack.add((byte) (y & Y_ANTIBIT));
+        pathQueue.add((byte) (y & Y_ANTIBIT));
     }
 
     public abstract byte travelX(byte amount);
@@ -28,8 +28,8 @@ public abstract class PlannedController extends MapAwareController {
     public abstract byte travelY(byte amount);
 
     public final void travelPath(){
-        while(pathStack.nonEmpty()){
-            final byte command = pathStack.remove();
+        while(pathQueue.nonEmpty()){
+            final byte command = pathQueue.remove();
             final boolean onX = (command & X_BIT) == X_BIT;
             final byte amount = (byte) ((command << 1) >> 1);
             if(onX){
