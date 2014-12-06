89aa1438d27bbd5c0002a73e53a943ac0fca0701
Darkyenus <darkyenus@gmail.com>
2014-12-06 18:01:46 +0100

    Added BOT_LOST event, that is not called from anywhere yet

--- shared/src/main/java/lego/api/BotEvent.java
+++ shared/src/main/java/lego/api/BotEvent.java
@@ -3,5 +3,16 @@ package lego.api;
 
 /** Private property. User: Darkyen Date: 19/10/14 Time: 13:43 */
 public enum BotEvent {
-	RUN_PREPARE, RUN_STARTED, RUN_ENDED
+	/** Called before anything else on high priority thread.
+	 * Do calculating, loading and similar preparation stuff here.
+	 * Should block until everything is completed. */
+	RUN_PREPARE,
+	/** Called after RUN_PREPARE. Should not block.
+	 * Robot shall start moving after this event. */
+	RUN_STARTED,
+	/** Called anytime, signalling that run is ended (for whatever reason) and everything should stop. */
+	RUN_ENDED,
+	/** Called when BotController determines, that the bot has done something incorrectly, and does no longer know where it is.
+	 * Bot can use this for change of plans. */
+	BOT_LOST
 }
